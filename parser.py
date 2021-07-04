fb096866b27552cdef317ed8c33e39e92f03ddec
ArthurSiqueiraS <asesilva@inf.ufpel.edu.br>
2021-07-04 00:37:15 -0300

    V2

--- parser.py
+++ parser.py
@@ -11,31 +11,250 @@ import xml.etree.ElementTree as ET
 import sys
 import re
 import json
+from abc import ABC, abstractmethod
 
-""" get_namespace
+class ParserXML(ABC):
+  def __init__(self, root):
+    """
+      Initialization
+    """
+    self.root = root
+    self.ns = ''
 
-  Gets XML namespace from element
+  def _get_namespace(self, element):
+    """
+      Gets XML namespace from element
+    """
+    ns = re.match(r'\{.*\}', element.tag)
+    return ns.group(0) if ns else ''
 
-"""
-def get_namespace(element):
-  ns = re.match(r'\{.*\}', element.tag)
-  return ns.group(0) if ns else ''
+  def _iter(self, tag, root=None):
+    """
+      Iterates over elements inside the given tree that match the tag
+    """
+    if not root:
+      root = self.root
+    return root.iter(self.ns + tag)
 
-""" abort
+  def _find(self, el, tag):
+    """
+      Finds the first child of the element with the specified tag
+    """
+    return el.find(self.ns + tag)
 
-  Prints and error message and interrupts execution
+  def _format_flight(self, id, date, time, rows):
+    """
+      Formats flight data to a standard dict format
+    """
+    return {
+      'flight_id': id,
+      'date': date,
+      'time': time[:5],
+      'rows': rows
+    }
 
-"""
+  def _format_row(self, id, cabin_class, seats=[]):
+    """
+      Formats a flight row data to a standard dic
+    """
+    return {
+      'row_id': id,
+      'cabin_class': cabin_class,
+      'seats': seats
+    }
+
+  def _format_seat(self, id, seat_type, available, price='N/A', taxes='N/A'):
+    """
+      Formats seat data to a standard dict
+    """
+    return {
+      'seat_id': id,
+      'type': seat_type,
+      'available': available,
+      'price': price,
+      'taxes': taxes
+    }
+
+  def _format_price(self, amount, currency='', precision=2):
+    """
+      Returns the price in a readable format
+    """
+    if amount > 0:
+      return '{price: .{prec}f} {curr}'.format(price = amount, prec = precision, curr = currency).strip()
+    else:
+      return 'N/A'
+
+  @abstractmethod
+  def parse(self):
+    """
+      Parses the XML file into a json output
+    """
+
+class SoapParser(ParserXML):
+  def parse(self):
+    """
+      Parses a soap type XML tree into and returns the relevant data in a standard structure
+    """
+    body = root[0][0]
+    # Saves namespace for iterations
+    self.ns = self._get_namespace(body)
+    result = {}
+
+    # Iterates over seatmap responses
+    for response in self._iter('SeatMapResponse', body):
+      # Flight info
+      flight = self._find(response, 'FlightSegmentInfo')
+      flight_id = flight.get('FlightNumber')
+      flight_departure = flight.get('DepartureDateTime')
+      [flight_date, flight_time] = flight_departure.split('T')
+
+      rows = {}
+
+      seatmap = self._find(response, 'SeatMapDetails')
+      # Iterates over flight cabin rows
+      for row in self._iter('RowInfo', seatmap):
+        row_data = self.__soap_row_info(row)
+        rows[row_data.get('row_id')] = row_data
+
+      result[flight_id] = self._format_flight(flight_id, flight_date, flight_time, rows)
+
+    return result
+
+  def __soap_row_info(self, row):
+    """
+      Receives a row XML element and extracts it's relevant data into a standard format
+    """
+    cabin_class = row.get('CabinType')
+    row_number = row.get('RowNumber')
+
+    seats = [self.__soap_seat_info(seat) for seat in self._iter('SeatInfo', row)]
+
+    return self._format_row(row_number, cabin_class, seats)
+
+  def __soap_seat_info(self, seat):
+    """
+      Receives a seat XML element and extracts it's relevant data into a standard format
+    """
+    summary = self._find(seat, 'Summary')
+    seat_id = summary.get('SeatNumber')
+    available = summary.get('AvailableInd') == 'true'
+    # Get seat features
+    seat_type = []
+    for feature in self._iter('Features', seat):
+      feat = feature.get('extension')
+      if not feat:
+        feat = feature.text
+      seat_type.append(feat)
+
+    # Get fee and tax prices
+    if available:
+      fee = self._find(self._find(seat, 'Service'), 'Fee')
+      amount = float(fee.get('Amount'))
+      precision = int(fee.get('DecimalPlaces'))
+      currency = fee.get('CurrencyCode')
+      seat_price = self._format_price((amount / (10**precision)), currency, precision)
+
+      total_taxes = sum([float(tax.get('Amount')) for tax in self._iter('Tax', fee)])
+      taxes = self._format_price((total_taxes / (10**precision)), currency, precision)
+    else:
+      seat_price = self._format_price(0)
+      taxes = self._format_price(0)
+
+    return self._format_seat(seat_id, seat_type, available, seat_price, taxes)
+
+class IataParser(ParserXML):
+  def parse(self):
+    """
+      Parses an iata type XML tree into and returns the relevant data in a standard structure
+    """
+    # Sets namespace
+    self.ns = self._get_namespace(self.root)
+    # Collects data for flights, offers and seat definitions
+    self.flights = { flight.get('SegmentKey'): self.__iata_flight_info(flight) for flight in self._iter('FlightSegment') }
+    self.offers = { offer.get('OfferItemID'): self.__iata_offer_info(offer) for offer in self._iter('ALaCarteOfferItem') }
+    self.seat_definitions = {}
+    for seat_def in self._iter('SeatDefinition'):
+      self.seat_definitions[seat_def.get('SeatDefinitionID')] = self._find(self._find(seat_def, 'Description'), 'Text').text
+
+    # Extracts data from each seatmap
+    for seat_map in self._iter('SeatMap'):
+      seg_ref = self._find(seat_map, 'SegmentRef').text
+      flight = self.flights.get(seg_ref)
+      flight['rows'] += [self.__iata_row_info(row) for row in self._iter('Row')]
+
+    return { flight.get('flight_id'): flight for flight in self.flights.values() }
+
+  def __iata_flight_info(self, flight):
+    """
+      Receives a Flight Segment element and extracts it's relevant data into a standard format
+    """
+    departure_info = self._find(flight, 'Departure')
+    flight_date = self._find(departure_info, 'Date').text
+    flight_time = self._find(departure_info, 'Time').text
+    flight_id = self._find(self._find(flight, 'MarketingCarrier'), 'FlightNumber').text
+    return self._format_flight(flight_id, flight_date, flight_time, [])
+
+  def __iata_offer_info(self, offer):
+    """
+      Receives an Offer Item element and extracts it's relevant data into a standard format
+    """
+    id = offer.get('OfferItemID')
+    total_amount_el = self._find(self._find(offer, 'UnitPriceDetail'), 'TotalAmount')
+    price_el = self._find(total_amount_el, 'SimpleCurrencyPrice')
+    amount = float(price_el.text)
+    currency = price_el.get('Code')
+    price = self._format_price(amount, currency)
+    return price
+
+  def __iata_row_info(self, row):
+    """
+      Receives a Row element and extracts it's relevant data into a standard format
+    """
+    row_number = self._find(row, 'Number').text
+    seats = [self.__iata_seat_info(seat, row_number) for seat in self._iter('Seat', row)]
+    return self._format_row(row_number, 'Common', seats)
+
+  def __iata_seat_info(self, seat, row_number):
+    """
+      Receives a Seat element and Row number and extracts it's relevant data into a standard format
+    """
+    col = self._find(seat, 'Column').text
+    seat_id = row_number + col
+
+    offer_ref = self._find(seat, 'OfferItemRefs')
+    try:
+      price = self.offers[offer_ref.text]
+    except AttributeError:
+      # Offer might not be referenced
+      price = self._format_price(0)
+
+    available = False
+    seat_type = []
+    # Lists referenced seat definitions
+    for seat_def_ref in self._iter('SeatDefinitionRef', seat):
+      key = seat_def_ref.text
+      if key == 'SD4':
+        # SD4 is definition AVAILABLE
+        available = True
+      else:
+        seat_type.append(self.seat_definitions.get(key))
+
+    return self._format_seat(seat_id, seat_type, available, price, self._format_price(0))
+
 def abort(msg):
+  """
+    Prints an error message and interrupts execution
+  """
   print("\033[91mERROR:\033[0m " + msg)
   exit()
 
-""" xml_to_json
+if __name__ == "__main__":
+  try:
+    path = sys.argv[1]
+  except IndexError:
+    # Abort if no argument passed
+    abort("Must pass a file path")
 
-  Parses the XML file into a json output
-
-"""
-def xml_to_json(path):
   try:
     tree = ET.parse(path)
   except FileNotFoundError:
@@ -46,165 +265,13 @@ def xml_to_json(path):
   root = tree.getroot()
 
   if 'xmlsoap' in root.tag:
-    return parse_from_soap(root)
+    parser = SoapParser(root)
   elif 'iata' in root.tag:
-    return parse_from_iata(root)
+    parser = IataParser(root)
   else:
     abort('XML format not supported')
 
-""" parse_from_soap
-
-  Parses a soap type XML tree into and returns the relevant data in a standard structure
-
-"""
-def parse_from_soap(root):
-  global ns
-  body = root[0][0]
-  # Saves namespace for iterations
-  ns = get_namespace(body)
-  result = {}
-
-  # Iterates over seatmap responses
-  for response in body.iter(ns+'SeatMapResponse'):
-    # Flight info
-    flight = response.find(ns+'FlightSegmentInfo')
-    flight_id = flight.get('FlightNumber')
-    flight_departure = flight.get('DepartureDateTime')
-
-    rows = {}
-
-    seatmap = response.find(ns+'SeatMapDetails')
-    # Iterates over flight cabin rows
-    for row in seatmap.iter(ns+'RowInfo'):
-      row_data = soap_row_info(row)
-      rows[row_data['row_id']] = row_data
-
-    result[flight_id] = format_flight(flight_id, flight_departure, rows)
-
-  return result
-
-""" soap_row_info
-
-  Receives a row XML element and extracts it's relevant data into a standard format
-
-"""
-def soap_row_info(row):
-  cabin_class = row.get('CabinType')
-  row_number = row.get('RowNumber')
-
-  seats = [soap_seat_info(seat) for seat in row.iter(ns+'SeatInfo')]
-
-  return format_row(row_number, cabin_class, seats)
-
-""" soap_seat_info
-
-  Receives a seat XML element and extracts it's relevant data into a standard format
-
-"""
-def soap_seat_info(seat):
-  summary = seat.find(ns+'Summary')
-  seat_id = summary.get('SeatNumber')
-  available = summary.get('AvailableInd') == 'true'
-  # Get seat features
-  seat_type = []
-  for feature in seat.iter(ns+'Features'):
-    try:
-      seat_type.append(feature.get('extension'))
-    except KeyError:
-      seat_type.append(feature.text)
-
-  # Get fee and tax prices
-  if available:
-    fee = seat.find(ns+'Service').find(ns+'Fee')
-    amount = int(fee.get('Amount'))
-    precision = int(fee.get('DecimalPlaces'))
-    currency = fee.get('CurrencyCode')
-    seat_price = format_price(amount, currency, precision)
-
-    total_taxes = sum([int(tax.get('Amount')) for tax in fee.findall(ns+'Tax')])
-    taxes = format_price(total_taxes)
-  else:
-    seat_price = format_price(0)
-    taxes = format_price(0)
-
-  return format_seat(seat_id, seat_type, available, seat_price, taxes)
-
-""" parse_from_iata
-
-  Parses an iata type XML tree into and returns the relevant data in a standard structure
-
-"""
-def parse_from_iata(root):
-  global ns
-  ns = get_namespace(root)
-  # fligts_ref = { flight.get(eg: )
-  offers = { offer.get('OfferItemID'): iata_offer_info(offer) for offer in root.iter(ns+'ALaCarteOfferItem') }
-  return {}
-
-def iata_offer_info(offer):
-  id = offer.get('OfferItemID')
-  # price_el = offer.find('UnitPriceDetail').find('TotalAmount').find('SimpleCurrencyPrice')
-  # amount = price_el.text
-  # currency = price_el.get('Code')
-  # price = format_price(amount, currency)
-
-""" format_flight
-
-  Formats flight data to a standard dict format
-
-"""
-def format_flight(id, departure, rows):
-  return {
-    'flight_id': id,
-    'departure': departure,
-    'rows': rows
-  }
-
-""" format_row
-
-  Formats a flight row data to a standard dict
-
-"""
-def format_row(id, cabin_class, seats=[]):
-  return {
-    'row_id': id,
-    'cabin_class': cabin_class,
-    'seats': seats
-  }
-
-""" format_seat
-
-  Formats seat data to a standard dict
-
-"""
-def format_seat(id, seat_type, available, price='N/A', taxes='N/A'):
-  return {
-    'seat_id': id,
-    'type': seat_type,
-    'available': available,
-    'price': price,
-    'taxes': taxes
-  }
-
-""" format_price
-
-  Returns the price in a readable format
-
-"""
-def format_price(amount, currency='', precision=2):
-  if amount > 0:
-    return '{price: .{prec}f} {curr}'.format(price = (amount / (10**precision)), prec = precision, curr = currency)
-  else:
-    return 'N/A'
-
-if __name__ == "__main__":
-  try:
-    path = sys.argv[1]
-  except IndexError:
-    # Abort if no argument passed
-    abort("Must pass a file path")
-
-  data = xml_to_json(path)
+  data = parser.parse()
   output_filename = '{FILENAME}_parsed.json'.format(FILENAME = path.split('.')[0])
 
   with open(output_filename, 'w') as f:
